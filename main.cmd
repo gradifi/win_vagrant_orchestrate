0beed54f147356f916b2ed3bedaa695e1f8fde6f
Tommy Parnell <tparnell8@gmail.com>
2016-03-25 20:24:00 -0400

    remove unused script

--- main.cmd
+++
@@ -1,5 +0,0 @@
-echo 'Ensuring Chocolatey is Installed'
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-
-echo 'Ensuring Puppet is Installed'
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "choco install puppet -y"
